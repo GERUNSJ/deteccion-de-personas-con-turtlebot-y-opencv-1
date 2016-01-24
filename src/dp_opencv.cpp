18559d1305d18a3a6080d47ac11f4868b7662fd4
Pablo Aguado <aguadopd@hotmail.com>
2016-01-24 13:24:32 -0300

    Comienzo programa general.

---
+++ src/dp_opencv.cpp
@@ -0,0 +1,22 @@
+
+
+//----------------------------------------------------------------------------------------------
+// Includes
+
+
+//----------------------------------------------------------------------------------------------
+// MAIN
+
+int main(int argc, char** argv)
+{
+  // Ayuda de uso
+  
+  // Se lee la carpeta con las imagenes, o la imagen según corresponda
+  
+  // Se crea un detector
+  
+  // Se procesan las imagenes
+  
+  // Se guardan los resultados
+}
+
