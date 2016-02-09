8e8c5e541936f197f541ab47bf1f659595f43c7b
Pablo Aguado <aguadopd@hotmail.com>
2016-02-09 20:33:06 -0300

    Faltan pruebas. Lee archivos y procesa. No escribe.

--- src/dp_resultados.cpp
+++ src/dp_resultados.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <cstdio>
 #include "resultados.hpp"
 
 using namespace std;
@@ -11,7 +12,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	string i_reales;	// Ruta y nombre del archivo de los datos reales, con extensión
-	string i_estimados;	// Ruta y nombre del archivo de los datos estimados, con extensión
+	string i_estimados;	// Ruta y nombre del archivo de los daots estimados, con extensión
 	string i_resultados;// Ruta y nombre del archivo de los resultados generados, con extensión
 
 	if( argc != 4 )
@@ -21,11 +22,152 @@ int main(int argc, char* argv[])
 	i_estimados = argv[2];
 	i_resultados = argv[3];
 
-	fstream stream_reales, stream_estimados, stream_resultados;
-	stream_reales.open(i_reales.c_str(), ios::in );
-//
-//	string aux = i_nombre_archivos_resultados + ".csv"; // Append con +
-//	stream_archivo_csv.open(aux.c_str(), ios::out | ios::app); // Salida, append(añadir)
+//	fstream stream_reales, stream_estimados, stream_resultados;
+//	stream_reales.open(i_reales.c_str(), ios::in );
+//	stream_estimados.open(i_estimados.c_str(), ios::in);
+//	stream_resultados.open(i_resultados.c_str(), ios::out);
+	FILE* arch_reales;
+	FILE* arch_estimados;
+	fstream stream_resultados;
 
+	arch_reales = fopen(i_reales.c_str(), "r");
+	arch_estimados = fopen(i_estimados.c_str(), "r");
+	stream_resultados.open(i_resultados.c_str(), ios::out);
+	// TODO: Chequear si se abrieron bien
+
+	//-----
+	vector<struct_resultados> reales;
+	vector<struct_resultados> estimados;
+	vector<Frame> frames;
+
+	unsigned int max = 0;
+
+// TODO detecciones multiples...quien las elimina? resultados o detector?
+
+	//Set;Imagen;Profundidad;Completa;Tiempo;Arriba_izq_X;Arriba_izq_Y;Arriba_der_X;Arriba_der_Y;Abajo_izq_X;Abajo_izq_Y;Abajo_der_X;Abajo_der_Y;
+
+	struct_resultados aux;
+	//char* aux_string = nullptr;
+	char auxs[50];
+
+	// Lectura reales
+	while( !feof(arch_reales) )
+	{
+		fscanf(arch_reales, "%[^;];%u;%u;%d;%f;%u;%u;%u;%u;%u;%u;%u;%u \n",
+			auxs, &aux.img, &aux.prof, &aux.comp, &aux.tiempo, &aux.arr_izq_x, &aux.arr_izq_y,
+			&aux.arr_der_x, &aux.arr_der_y, &aux.aba_izq_x, &aux.aba_izq_y, &aux.aba_der_x, &aux.aba_der_y);
+
+		aux.set = auxs;
+
+		//cout << aux;
+		aux.calcular(); // Calculamos centro, ancho, alto
+		reales.push_back(aux);
+
+		if( aux.img > max )
+			max = aux.img;
+	}
+	fclose(arch_reales);
+
+
+	// Lectura estimados
+	while( !feof(arch_estimados) )
+	{
+		fscanf(arch_estimados, "%[^;];%u;%u;%d;%f;%u;%u;%u;%u;%u;%u;%u;%u \n",
+			auxs, &aux.img, &aux.prof, &aux.comp, &aux.tiempo, &aux.arr_izq_x, &aux.arr_izq_y,
+			&aux.arr_der_x, &aux.arr_der_y, &aux.aba_izq_x, &aux.aba_izq_y, &aux.aba_der_x, &aux.aba_der_y);
+
+		aux.set = auxs;
+
+		//cout << aux;
+		aux.calcular(); // Calculamos centro, ancho, alto
+		estimados.push_back(aux);
+	}
+	fclose(arch_estimados);
+
+
+	cout << "\nSe han leído " << reales.size() << " líneas de datos reales\n"
+			"y se han le[ido " << estimados.size() << " líneas de datos estimados.\n";
+
+
+	// Asignación de reales y estimados a cada frame
+	cout << "\nmax = " << max ;
+	frames.resize(max);	// Reservamos espacio
+	for( unsigned int i = 0 ; i < frames.size() ; i++ )
+	{
+		for( auto r : reales)
+		{
+			if( r.img == (i+1))
+				frames.at(i).reales.push_back(r);
+		}
+		for( auto e : estimados )
+		{
+			if( e.img == (i+1) )
+				frames.at(i).estimados.push_back(e);
+		}
+	}
+	//TODO: no estamos avisando si hay datos que no corresponden a ninguna imagen.
+
+
+//	for(  unsigned int i = 0 ; i < frames.size() ; i++  )
+//	{
+//		cout << "\nframe: " << i << "\treales: " << frames.at(i).reales.size() <<
+//				"\testimados: " << frames.at(i).estimados.size();
+//	}
+
+
+	// Suponemos que todas las imagenes son del mismo set..
+	// Evaluación: en cada frame comparamos:
+	for( auto& f : frames )	// Range-based for. Atento al & para pasar por referencia.
+	{
+		if( f.reales.size() > 0 )
+		{
+			// A cada persona real
+			for( auto& r: f.reales)
+			{
+				// la comparamos con todas las detecciones
+				for( auto& e: f.estimados )
+				{
+					// y si es correcto
+					if( r == e )
+					{
+						//cout << "\n iguales";
+						f.verdaderos_positivos += 1;
+						// eliminamos el elemento del vector
+						f.estimados.pop_back();
+					}
+				}
+				// una vez comparado con todos, eliminamos el elemento del vector
+				f.reales.pop_back();
+			}
+		}
+		// y al final:
+		f.falsos_positivos = f.estimados.size();
+		f.falsos_negativos = f.reales.size();
+		cout << "\nVerdaderos positivos = " << f.verdaderos_positivos;
+		cout << "\nFalsos positivos = " << f.falsos_positivos;
+		cout << "\nFalsos negativos = " << f.falsos_negativos;
+	}
+
+	cout << "\n\n\n\n-----------\n";
+
+	unsigned int vpos = 0;
+	unsigned int fpos = 0;
+	unsigned int fneg = 0;
+
+	for( auto f: frames )
+	{
+		vpos += f.verdaderos_positivos;
+		fpos += f.falsos_positivos;
+		fneg += f.falsos_negativos;
+	}
+
+	cout << "\nVerdaderos positivos = " << vpos;
+	cout << "\nFalsos positivos = " << fpos;
+	cout << "\nFalsos negativos = " << fneg;
+
+	// TODO: Qué hacemos con las personas incompletas?
+
+	stream_resultados.close();
+	cout << "\nEl programa ha terminado.\n\n";
 	return 0;
 }
--- src/resultados.cpp
+++ src/resultados.cpp
@@ -21,3 +21,25 @@ void rect_a_struct_resultados(const cv::Rect rect, struct_resultados& res)
     res.arr_izq_x = rect.x;
     res.arr_izq_y = rect.y;
 }
+
+
+void struct_resultados::calcular()
+{
+	ancho = aba_der_x - aba_izq_x + 1;
+	alto = aba_der_y - arr_der_y + 1;
+
+	centro_x = ancho/2;
+	centro_y = alto/2;
+}
+
+
+// Importante el orden de los operadores..
+bool operator==(const struct_resultados& real, const struct_resultados& est)
+{
+	float distancia = sqrt( powf((real.centro_x-est.centro_x),2) + powf((real.centro_y-est.centro_y),2) );
+	if( distancia < P_RADIO*real.ancho )
+		if( est.ancho < P_ANCHO*real.ancho )
+			if( est.alto < P_ALTO*real.alto )
+				return true;
+	return false;
+}
