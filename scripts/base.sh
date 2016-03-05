a86083c351c0bd7e6b97e4c8884a3da4479f102e
Pablo Aguado <aguadopd@hotmail.com>
2016-03-05 18:07:25 -0300

    Funciona. Script base y detectorfinal. Puede mejorar.

--- scripts/base.sh
+++ scripts/base.sh
@@ -8,7 +8,9 @@ CARPETA_RGB=./../../dp_databases/set1_color
 # Carpeta con imagenes de profundidad
 CARPETA_PROFUNDIDAD=./../../dp_databases/set1_depth16
 # Carpeta con los resultados
-CARPETA_RESULTADOS=./../resultados/
+CARPETA_RESULTADOS=./../resultados
+# Archivo con las coordenadas reales de las personas en las imágenes
+NOMBRE_REALES=reales
 
 #dp_opencv  <carpeta/con/imagenes/rgb>
 #                <carpeta/con/imagenes/profundidad | 0 >
@@ -16,17 +18,7 @@ CARPETA_RESULTADOS=./../resultados/
 #                <(1 | 0>(mostrar_detecciones)
 #                <clase_de_detector> [parámetro_1_nombre parámetro_1_valor ...]
 
-# DetectorFinal direccion_a_cascada string escala_inicial float convertir_a_gris bool ecualizar_histograma bool scaleFactor float minNeighbors int usar_profundidad_altura bool
-
-CASCADA=./../clasificadores/visionary.net_pedestrian_cascade_web_LBP.xml
-
-# DetectorFinal
-NOMBRE_RESULTADOS=final
-$PRINCIPAL $CARPETA_RGB $CARPETA_PROFUNDIDAD $CARPETA_RESULTADOS/$NOMBRE_RESULTADOS 0 DetectorFinal direccion_a_cascada $CASCADA
-
 #Uso: dp_resultados /ruta/a/datos_reales (sin extensión)
 #                   /ruta/a/datos_estimados (sin extensión)
 #                   /ruta/a/archivo_de_resultados.txt (con extensión)
 
-ARCHIVO_RESULTADOS=rfinal
-$RESULTADOS $CARPETA_RESULTADOS/$NOMBRE_RESULTADOS $CARPETA_RESULTADOS/$NOMBRE_RESULTADOS $CARPETA_RESULTADOS/$ARCHIVO_RESULTADOS.txt
